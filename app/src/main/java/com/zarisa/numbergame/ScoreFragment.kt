ff3bea25f663bf90afb13da58288a8737a293dcb
Zarisa <jannesari.zarisa@gmail.com>
2022-03-30 16:42:38 +0430

    delete an extra dependency and fix a bug

--- app/src/main/java/com/zarisa/numbergame/ScoreFragment.kt
+++ app/src/main/java/com/zarisa/numbergame/ScoreFragment.kt
@@ -1,14 +1,13 @@
 package com.zarisa.numbergame
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
-import android.support.v4.app.ActivityCompat.finishAffinity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat.finishAffinity
 import androidx.navigation.fragment.findNavController
 import com.zarisa.numbergame.databinding.FragmentScoreBinding
 
