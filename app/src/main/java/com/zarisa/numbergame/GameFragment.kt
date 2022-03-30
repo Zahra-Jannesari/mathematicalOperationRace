ba68bd90032f44f697df3da986ad468a9d8cd7cd
Zarisa <jannesari.zarisa@gmail.com>
2022-03-30 16:53:14 +0430

    move game logic from main activity to game fragment

--- app/src/main/java/com/zarisa/numbergame/GameFragment.kt
+++ app/src/main/java/com/zarisa/numbergame/GameFragment.kt
@@ -1,59 +1,156 @@
 package com.zarisa.numbergame
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.zarisa.numbergame.databinding.ActivityMainBinding
+import com.zarisa.numbergame.databinding.FragmentGameBinding
+import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [GameFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+@SuppressLint("SetTextI18n")
 class GameFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+   lateinit var binding:FragmentGameBinding
 
+    private var listButtons = mutableListOf<Button>()
+    //    lateinit var countDownTimer: CountDownTimer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false)
+    ): View {
+        binding= FragmentGameBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GameFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GameFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+    }
+    private fun initView() {
+        listButtons.add(binding.button1)
+        listButtons.add(binding.button2)
+        listButtons.add(binding.button3)
+        listButtons.add(binding.button4)
+        binding.buttonDice.setOnClickListener {dice()}
+        listButtons.forEach {
+            it.setOnClickListener {
+                checkAnswer(listButtons.indexOf(it))
             }
+        }
+        configChangeHandling()
+    }
+
+    private fun configChangeHandling() {
+        if (State.choiceButtonTrue) {
+            listButtons[State.randomIndex].setBackgroundColor(Color.GREEN)
+            listButtons.forEach { it.isClickable = false }
+        }
+        if (State.choiceButtonFalse) {
+            listButtons[State.buttonIsWrong].setBackgroundColor(Color.RED)
+            listButtons.forEach { it.isClickable = false }
+        }
+        binding.textViewScore.text ="Score: ${State.score}"
+        binding.textViewRecord.text="Record: ${State.record}"
+        if(State.GroupVisibility)
+            binding.groupAnswers.visibility=View.VISIBLE
+        else
+            binding.groupAnswers.visibility=View.GONE
+        listButtons.forEach { it.text=State.buttonList[listButtons.indexOf(it)] }
+        binding.textViewNumberA.text=State.numberA
+        binding.textViewNumberB.text=State.numberB
+    }
+
+    private fun dice() {
+        if (State.level >= 5) {
+            findNavController().navigate(R.id.action_gameFragment_to_scoreFragment)
+        }
+        else {
+//            countDownTimer.cancel()
+//            timerFun()
+            State.level++
+            listButtons.forEach { it.isClickable = true }
+            clearColor()
+            listButtons.forEach { it.text = "" }
+            var randomA = Random().nextInt(99) + 1
+            State.numberA = randomA.toString()
+            binding.textViewNumberA.text = State.numberA
+            var randomB = Random().nextInt(9) + 1
+            State.numberB = randomB.toString()
+            binding.textViewNumberB.text = State.numberB
+            State.randomIndex = Random().nextInt(listButtons.size - 1)
+            var div = divide(randomA, randomB)
+            listButtons[State.randomIndex].text = div.toString()
+            var listTextCheck = mutableListOf<String>()
+            listButtons.forEach {
+                if (listButtons.indexOf(it) == State.randomIndex)
+                    listTextCheck.add(div.toString())
+                else {
+                    if (it.text == "") {
+                        while (it.text == "") {
+                            var randAnswer = (Random().nextInt(9) + 1)
+                            if (!listTextCheck.contains(randAnswer.toString())&&randAnswer!=div) {
+                                listTextCheck.add(randAnswer.toString())
+                                it.text = (randAnswer.toString())
+                            }
+                        }
+                    }
+                }
+                binding.groupAnswers.visibility = View.VISIBLE
+                State.GroupVisibility = true
+                State.buttonList = listTextCheck
+            }
+            State.choiceButtonTrue = false
+            State.choiceButtonFalse = false
+            State.buttonIsWrong = 0
+        }
+    }
+
+//    private fun timerFun() {
+//        countDownTimer=object : CountDownTimer(10000, 1000) {
+//
+//            override fun onTick(millisUntilFinished: Long) {
+//                binding.textViewTimer.setText( "time: " + millisUntilFinished / 1000)
+//            }
+//            override fun onFinish() {
+//                Toast.makeText(this@MainActivity,"Time done!",Toast.LENGTH_SHORT).show()
+//                dice()
+//            }
+//        }.start()
+//    }
+
+    fun checkAnswer(butIndex: Int) {
+        if (butIndex == State.randomIndex) {
+            State.score += 5
+            listButtons[butIndex].setBackgroundColor(Color.GREEN)
+            State.choiceButtonTrue=true
+        } else {
+            State.score -= 2
+            listButtons[butIndex].setBackgroundColor(Color.RED)
+            State.choiceButtonFalse=true
+            State.buttonIsWrong=butIndex
+        }
+        binding.textViewScore.text="Score: ${State.score}"
+        listButtons.forEach { it.isClickable=false }
+
+    }
+    private fun divide(randomA: Int, randomB: Int): Int {
+        return randomA % randomB
+    }
+    private fun clearColor() {
+        listButtons.forEach {
+            it.setBackgroundColor(Color.BLUE)
+        }
     }
 }
