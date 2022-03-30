01fa7b7a649e920d401c4b5c16d439be2e487aee
Zarisa <jannesari.zarisa@gmail.com>
2022-03-30 15:10:04 +0430

    primary setup for start game fragment, features Not yet implemented.

--- app/src/main/java/com/zarisa/numbergame/StartGameFragment.kt
+++ app/src/main/java/com/zarisa/numbergame/StartGameFragment.kt
@@ -5,55 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import com.zarisa.numbergame.databinding.FragmentStartGameBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [StartGameFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+//for now its just for start, later we add optional range and operation
 class StartGameFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    lateinit var binding: FragmentStartGameBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start_game, container, false)
+    ): View {
+        binding = FragmentStartGameBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StartGameFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StartGameFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //TODO
+        //Chose a good name for this fragment
+        (activity as AppCompatActivity).supportActionBar?.title = "Start"
+        initViews()
+        foo()
+    }
+
+    private fun foo() {
+        TODO("")
+    }
+
+    private fun initViews() {
+        binding.buttonStart.setOnClickListener { findNavController().navigate(R.id.action_startGameFragment_to_gameFragment) }
     }
 }
